0b8e19f335ef3777566cc20d497db75425c70f09
harry <harrymac1972@gmail.com>
2024-03-29 22:28:51 -0600

    dashboard added

--- proj_chatgpt/app_chatgpt/views.py
+++ proj_chatgpt/app_chatgpt/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render
 
 def home(request):
@@ -5,3 +6,6 @@ def home(request):
 
 def about(request):
     return render(request,'about.html',{})
+
+def dashboard(request):
+    return render(request,'dashboard.html',{})
