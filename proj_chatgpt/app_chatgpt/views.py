977d031722b27f7967bf2e5d4756799211aec661
harry <harrymac1972@gmail.com>
2024-03-30 11:59:23 -0600

    home.html form submitting question to view and rendering the returned variable

--- proj_chatgpt/app_chatgpt/views.py
+++ proj_chatgpt/app_chatgpt/views.py
@@ -2,7 +2,11 @@
 from django.shortcuts import render
 
 def home(request):
+    if request.method == "POST":
+        question = request.POST['question']
+        return render(request,'home.html',{'question':question})
     return render(request,'home.html',{})
+    
 
 def about(request):
     return render(request,'about.html',{})
