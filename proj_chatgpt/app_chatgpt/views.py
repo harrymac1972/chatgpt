9670bed2a976643a5556c4e47b5e3dd60e67c18a
harry <harrymac1972@gmail.com>
2024-03-30 13:24:09 -0600

    get_api_key() added

--- proj_chatgpt/app_chatgpt/views.py
+++ proj_chatgpt/app_chatgpt/views.py
@@ -1,10 +1,15 @@
 
 from django.shortcuts import render
+import openai
+
 
 def home(request):
     if request.method == "POST":
         question = request.POST['question']
-        return render(request,'home.html',{'question':question})
+        api_key = get_api_key()
+        openai.api_key = api_key
+        return render(request,'home.html',{'question':question,
+                                            'api_key':api_key})
     return render(request,'home.html',{})
     
 
@@ -16,3 +21,11 @@ def dashboard(request):
 
 def prices(request):
     return render(request,'prices.html',{})
+
+# workaround to get environment variable for now
+def get_api_key():    
+    key_h = r"\Users\harry\open_ai_key.txt"
+    with open(key_h, 'r') as file:
+        key_v = file.read()
+    return key_v
+
